f410c9f12e5add2fd6560a6ae9c7aed856257a8e
Myron Mosaad <myronmosaad@gmail.com>
2020-06-17 00:35:10 -0400

    "added some logging"

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -11,10 +11,7 @@ import org.apache.log4j.LogManager;
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class.getName());
 
-
     public static void main( String[] args ){
-
-
 
         MainMenu menu = new MainMenu();
         menu.start();
